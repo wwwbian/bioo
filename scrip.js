aa27387fd33c642ba9cb5360d258937267ec67ae
DDHGDBNHDUEU <mbrodriguez@escuelasproa.edu.ar>
2023-08-22 11:19:11 -0300

    olaaaa

--- scrip.js
+++ scrip.js
@@ -1,9 +1,9 @@
 document.getElementById('BotonGenerador').addEventListener('click', function () {
-    let dnaChars = 'ACGT';
+    let Caracteres= 'ACGT';
     let secuenciaADN= '';
-    for (var i = 0; i < 18; i++) {
-        let randomIndex = Math.floor(Math.random() * dnaChars.length);
-        secuenciaADN += dnaChars.charAt(randomIndex);
+    for (let i = 0; i < 18; i++) {
+        let randomIndex = Math.floor(Math.random() * Caracteres.length);
+        secuenciaADN += Caracteres.charAt(randomIndex);
     }
     document.getElementById('secuenciaADN').textContent = 'Secuencia de ADN generada: ' + secuenciaADN;
 });
