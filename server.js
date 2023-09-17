d993163cb20f507550742bd9296fedfad31fe576
wwwbian <bdiaz@escuelasproa.edu.ar>
2023-09-17 18:30:28 -0300

    intenté hacer la base de datos (no pude)

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+mongoose.connect('mongodb+srv://maymay:ireneydraco@biologia.pkvneqx.mongodb.net/Biologia')
+.then(()=>{
+console.log('Conexion a mongoose lista')
+})
+.catch((err)=>{
+    console.log('Error de conexion a mongoose:', err)
+    })
+    
 
 app.listen(port, () => {
-    console.log(`el servidor esta en el puerto ${port}`);
-});
+  console.log(`el servidor esta en el puerto ${port}`);
+    });
